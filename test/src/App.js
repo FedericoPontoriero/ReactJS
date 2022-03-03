2bfc964e1805e25769fee8f562f4be643bea2362
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-03-03 18:59:52 -0300

    Modificado test por tasks

--- test/src/App.js
+++ test/src/App.js
@@ -1,68 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import tasks from "./sample/tasks.json";
+import Tasks from "./components/Tasks";
 
-/* function Helloworld(props) {
-	console.log(props);
-	return (
-		<div id="hello">
-			<h3>{props.subtitle}</h3>
-			{props.mytext}
-		</div>
-	);
-} */
-
-// Con class
-
-class Helloworld extends React.Component {
+class App extends Component {
 	state = {
-		show: true,
+		tasks: tasks,
 	};
-
-	toggleShow = () => {
-		this.setState({ show: !this.state.show });
-	};
-
 	render() {
-		if (this.state.show) {
-			return (
-				<div id="hello">
-					<h3>{this.props.subtitle}</h3>
-					{this.props.mytext}
-					<button onClick={this.toggleShow}>Toggle Show</button>
-				</div>
-			);
-		} else {
-			return (
-				<h1>
-					No hay elementos
-					<button onClick={this.toggleShow}>Toggle Show</button>
-				</h1>
-			);
-		}
+		return (
+			<div>
+				<Tasks tasks={this.state.tasks} />
+			</div>
+		);
 	}
-}
-
-// Con Arrow Functions
-
-// const App = () => <div>This is my component: <Helloworld /></div>
-
-// Con class
-
-// class App extends React.Component {
-//  render() {
-//    return <div>This is my component <Helloworld /></div>
-//  }
-// }
-
-function App() {
-	return (
-		<div>
-			This is my component:
-			<Helloworld mytext="Hello Fede" subtitle="lorem ipsum" />
-			<Helloworld mytext="Hola Mundo" subtitle="asd" />
-			<Helloworld mytext="Hi!" subtitle="ñalsdkl" />
-		</div>
-	);
 }
 
 export default App;
